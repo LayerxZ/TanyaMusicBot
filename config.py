6ac46609ac93f678e10b56e85a3cbb9c67aff791
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-08-05 11:54:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
 
 admins = {}
 API_ID = int(getenv("API_ID"))
