fd67fd404a0baa7bd699f1b29a2f4cfa4c9fdd49
primzzhz <86330267+primzzhz@users.noreply.github.com>
2021-08-12 22:33:13 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -22,7 +22,7 @@ from youtube_dl.utils import (
 
 @Client.on_message(filters.command("song") & ~filters.edited)
 async def song(client, message):
-    cap = "@trshszz_bot"
+    cap = "@trshzz_bot"
     url = message.text.split(None, 1)[1]
     rkp = await message.reply("Processing...")
     if not url:
