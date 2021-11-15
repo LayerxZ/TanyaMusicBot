04ae27adc4f08976cdb8d4a69c470a225ce9d0ad
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-15 15:42:31 +0700

    jadul

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -1,6 +1,7 @@
 from os import path
 
 from yt_dlp import YoutubeDL
+import yt_dlp
 
 from config import DURATION_LIMIT
 from helpers.errors import DurationLimitError
@@ -15,14 +16,23 @@ ydl_opts = {
 ydl = YoutubeDL(ydl_opts)
 
 
-def download(url: str) -> str:
+def download(url: str, my_hook) -> str:
+    ydl_optssx = {
+        'format' : 'bestaudio/best',
+        "outtmpl": "downloads/%(id)s.%(ext)s",
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        'quiet': True,
+        'no_warnings': True,
+    }
     info = ydl.extract_info(url, False)
-    duration = round(info["duration"] / 60)
-
-    if duration > DURATION_LIMIT:
-        raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
-
-    ydl.download([url])
-    return path.join("downloads", f"{info['id']}.{info['ext']}")
+    try:
+        x = yt_dlp.YoutubeDL(ydl_optssx)
+        x.add_progress_hook(my_hook)
+        dloader = x.download([url])
+    except Exception as y_e:
+        return print(y_e)
+    else:
+        dloader
+    xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
+    return xyz
