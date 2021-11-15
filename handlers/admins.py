26a64d2b461784b7a32c3981d6dc0b79b52e7799
primzzhz <86330267+primzzhz@users.noreply.github.com>
2021-11-15 17:13:54 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -59,7 +59,7 @@ async def resume(_, message: Message):
         await message.reply_text(f"🎧 Voicechat Resumed by {checking}!")
 
 
-@Client.on_message(command(["end"]) & other_filters)
+@Client.on_message(command(["y"]) & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
